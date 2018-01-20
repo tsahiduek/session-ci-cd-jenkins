0950aa22222d37ec12edac4794d33f60bd5d6075
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-01-20 14:49:45 +0200

    added new stages to wflow

--- wflow.groovy
+++ wflow.groovy
@@ -1,5 +1,11 @@
 node ('opsschool-slaves'){
     currentBuild.result = "SUCCESS"
+
+    stage('Checkout'){
+
+        checkout scm
+    }
+    
     stage('Test'){
         def out = sh script: 'ls -ltrh', returnStdout: true
         out()
