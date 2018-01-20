fdb8b4ab092b529e0e583199f4f3460a582bf606
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-01-20 15:51:15 +0200

    added new stages to wflow

--- wflow.groovy
+++ wflow.groovy
@@ -7,7 +7,19 @@ node ('opsschool-slaves'){
     }
 
     stage('Test'){
-        sh 'ls -ltrha'
+        sh 'ruby app/tc_ruby_app.rb'
+
+        sh 'ls -ltrh test/reports/'
+    }
+
+    stage('Build'){
+        sh 'sudo docker build --no-cache -t localhost:5000/opsschool_dummy_app:latest .'
+        sh 'sudo docker push localhost:5000/opsschool_dummy_app:latest'
+        sh 'sudo docker-compose stop'
+
+        sh ''''if [ ! "$(sudo docker ps -q -f name=opsschool_dummy_app)" ]; then\n' +
+                '\tsudo docker rm -f opsschool_dummy_app\n' +
+                'fi'''
     }
 
 }
