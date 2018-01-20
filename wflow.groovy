9fe0e5fd33a919fdc7f4e6de0e5942da06f51650
arie.belenky <ariebelenky@Aries-MacBook-Pro.local>
2018-01-20 14:43:03 +0200

    added new stages to wflow

--- wflow.groovy
+++ wflow.groovy
@@ -1,7 +1,7 @@
 node ('opsschool-slaves'){
     currentBuild.result = "SUCCESS"
     stage('Test'){
-        sh 'ruby app/tc_ruby_app.rb'
+        //sh 'ruby app/tc_ruby_app.rb'
         sh 'ls -ltrh test/reports/'
     }
 
